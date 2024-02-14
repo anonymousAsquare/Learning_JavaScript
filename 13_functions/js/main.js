0627c64f0104e2abcc536c598b58654b603466f2
anonymousAsquare <alainengiyaalainengiya@gmail.com>
2024-02-14 09:02:12 +0100

    day 4

---
+++ 13_functions/js/main.js
@@ -0,0 +1,19 @@
+// Theres are 3 ways to define a function in js
+// 1
+function addition(a,b){
+    return a + b;
+}
+
+console.log(addition(1,2));
+
+// 2
+subtraction = function(a,b){
+    return a - b;
+}
+console.log(subtraction(3,2));
+
+// 3
+multiplication = (a,b) =>{
+    return a * b;
+}
+console.log(multiplication(2,3));
---
+++ 14_var_let_const/js/main.js
@@ -0,0 +1,13 @@
+// var
+var x = 3;
+var x = 4;
+
+// let
+let y = 4;
+// let y = 5; error
+y = 5;
+
+// const
+const z = 5;
+// const z = 6; error
+// z = 7; error
